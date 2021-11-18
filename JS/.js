c5de64e4c58ec69c1fbd36800b908a36022a4340
jacopo35 <jaczor00@gmail.com>
2021-11-18 14:54:54 +0100

    description

---
+++ JS/.js
@@ -0,0 +1,11 @@
+
+/*
+Descrizione esercizio: 
+1. Chiedi all’utente il suo nome,
+2. poi chiedi il suo cognome,
+3. poi chiedi il suo colore preferito
+4. Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+
+
