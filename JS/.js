cf0eafc3ae676600908aa265a55504e84ba266ff
jacopo35 <jaczor00@gmail.com>
2021-11-18 15:36:32 +0100

    exercise-finished

--- JS/.js
+++ JS/.js
@@ -8,4 +8,25 @@ Descrizione esercizio:
 */
 
 
+const name = prompt("come ti chiami?");
+console.log(name);
 
+let cognome = "ottimo! Ora il tuo cognome!";
+let allerta = " sono nell'alert"
+
+const surname = prompt(cognome);
+console.log(surname);
+
+let color = prompt("continua cosi! Il tuo colore preferito? ");
+console.log(color);
+
+let numero = 10;
+numero += 11;
+
+
+let somma = `Ricapitolando ti chiami ${name} ${surname}, il tuo colore preferito è  ${color}  e il tuo numero direi che potrebbe essere il...${numero}`;
+
+document.getElementById("js").innerHTML = somma + " sono nel getelement";
+document.writeln(somma + " sono nel writeln");
+alert(somma + allerta);
+console.log(somma + "sono nella console");
